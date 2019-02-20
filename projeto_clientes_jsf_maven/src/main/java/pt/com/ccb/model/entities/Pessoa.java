3b3f2ea9effd976d8bbf16a1341cd7c7d91cfd31
Cesar Barbosa <Cesar Barbosa@10.1.40.86>
2019-02-20 12:15:34 +0000

    finalização da criação das entidades

--- projeto_clientes_jsf_maven/src/main/java/pt/com/ccb/model/entities/Pessoa.java
+++ projeto_clientes_jsf_maven/src/main/java/pt/com/ccb/model/entities/Pessoa.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -46,6 +47,7 @@ public class Pessoa implements Serializable{
 
     @ManyToOne(optional = false)
     @ForeignKey(name="PessoaSexo")
+    @JoinColumn(name="IdSexo", referencedColumnName = "IdSexo")
     private Pessoa pessoa;
     
     public Pessoa() {
